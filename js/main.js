c4be48c7ed2a95829b1c79ecf592cbe27c8e319a
RITSUYA2020 <12ritsuya23@gmail.com>
2021-02-01 09:40:44 +0900

    [Finish]動作確認

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ var app = new Vue({
         getAnswer: function() {
             if(this.keyword === '') {
                 this.items = null
+                this.message = ''
                 return
             }
             this.message = 'Loading...'
